5795b8c6e0ab16b4992a85dc8d6eef8f064cbb09
Lynn Wang <lynncwang@gmail.com>
2016-05-30 23:55:05 -0700

    final changes

---
+++ public/scripts/providerid.js
@@ -0,0 +1,7 @@
+<script>
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+var day = days[ now.getDay() ];
+var month = months[ now.getMonth() ];
+</script>
