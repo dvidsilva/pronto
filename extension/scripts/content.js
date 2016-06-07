d0e3237dd3ae8c6d4862e950e2b75adcc98c9e27
David Silva <contacto@dvidsilva.com>
2016-06-07 10:13:54 -0700

    fixed checbox for no email

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -28,6 +28,9 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
             }
         }
     });
+    if (!data.EMAIL) {
+        $('[name=noemail]').click();
+    }
     sendResponse({data: data, success: true});
 });
 
